28cdf8f4a80eb8f435e4f99d2ae1b2454174de8b
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-02-18 18:07:32 +0000

    Created basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+
+// Created by: Vlad
+// Created on: 02-18-2025
+// This program just does math
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "5+5=" << 5+5 << std::endl;
+    std::cout << "5-5=" << 5-5 << std::endl;
+    std::cout << "5*5=" << 5*5 << std::endl;
+    std::cout << "5/5=" << 5 / 5 << std::endl;
+    std::cout << "5^3=" << pow(5, 3) << std::endl;
+    std::cout << "sqrt(4)=" << sqrt(4) << std::endl;
+}
